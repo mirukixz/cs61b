7d41a520ae2603d36567e4dfdd63b112a767e48d
YXZ <15307130350@fudan.edu.cn>
2019-09-18 23:32:28 +0800

    Body constructor

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Body {
+    public  double xxPos,yyPos,xxVel,yyVel,mass;
+    public  String imgFileName;
+    public Body(double xP,double yP,double xV,double yV,double m,String img){
+            xxPos=xP;
+            yyPos=yP;
+            xxVel=xV;
+            yyVel=yV;
+            mass=m;
+            imgFileName=img;
+    }
+    public Body(Body b){
+        xxPos=b.xxPos;
+        yyPos=b.yyPos;
+        xxVel=b.xxVel;
+        yyVel=b.yyVel;
+        mass=b.mass;
+        imgFileName=b.imgFileName;
+    }
 }
